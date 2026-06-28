6e567ea87706c4bc813ec954564b531a67b2d86a
Hector <8d6g6xpyxv@privaterelay.appleid.com>
2026-06-28 14:17:04 -0600

    Project creation

---
+++ HabitsTrackerTests/HabitsTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsTrackerTests.swift
+//  HabitsTrackerTests
+//
+//  Created by Héctor Beristain on 28/06/26.
+//
+
+import Testing
+@testable import HabitsTracker
+
+struct HabitsTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
