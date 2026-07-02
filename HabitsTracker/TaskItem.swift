b0416d3dd33628fa5c4275f81b6c427bf627a79e
Hector <8d6g6xpyxv@privaterelay.appleid.com>
2026-07-01 19:05:23 -0600

    XP reward logic in TaskItem

--- HabitsTracker/TaskItem.swift
+++ HabitsTracker/TaskItem.swift
@@ -13,11 +13,13 @@ final class TaskItem {
     var title: String
     var finished: Bool
     var nextTask: TaskItem?
+    var xpReward: Int
     
     init() {
         self.title = ""
         self.finished = false
         self.nextTask = nil
+        self.xpReward = 25
     }
 
     public func setState() {
@@ -30,5 +32,13 @@ final class TaskItem {
     
     public func getNextTask() -> String?{
         return self.nextTask?.title
+    }
+    
+    public func giveXp() -> Int {
+        if self.finished {
+            return self.xpReward
+        } else {
+            return 0
+        }
     }
 }
